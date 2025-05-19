67906a0ebcf963a57967ce76eb58cc50565012bb
YuriVasconcelosJ <yurivasconcelosavatar@gmail.com>
2025-05-19 15:29:09 -0300

    Produto criado

--- src/main/java/controller/FuncionarioController.java
+++ src/main/java/controller/FuncionarioController.java
@@ -2,4 +2,28 @@ package controller;
 
 public class FuncionarioController {
 
+	// Entrar no pdv
+	// finalizar venda
+	// Add produto
+	// retirar produto?
+	
+	// Logar
+	public void login() {/* bool */}
+	
+	public void deslogar() { /* void */ }
+	
+	// Vendas
+	public void iniciarVenda() { /* void */ }
+	
+	public void removerProduto() {/* string produto_id : void  */}
+	
+	public void atualizarQuantidadeProduto() {/* void */}
+	
+	public void totalCompra() {/* double */}
+	
+	public void finalizarVenda() {/* void  */}
+	
+	public void cancelarVenda() {/* void */ }
+	
+	
 }
