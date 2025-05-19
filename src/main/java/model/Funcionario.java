66f8abfbed6f7d27ac49bb7514fb5aea0c62d5b0
FELIPE SABINO DE OLIVEIRA LIMA <felipe.slima@a.ficr.edu.br>
2025-05-19 14:32:36 -0300

    adicionado: classes no pacote model

--- src/main/java/model/Funcionario.java
+++ src/main/java/model/Funcionario.java
@@ -1,5 +1,104 @@
 package model;
 
-public class Funcionario {
+import java.util.Date;
 
+public abstract class Funcionario {
+	
+	private int id;
+	private int proximoId;
+	private String nome;
+	private int cpf;
+	private Date dataNascimento;
+	private int telefone;
+	private String email;
+	private String endereco;
+	private Date dataAdmissao;
+	private String cargo;
+	private double salario;
+	private String senha;
+	private boolean ativo;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	/*public int getProximoId() {
+		return proximoId;
+	}
+	public void setProximoId(int proximoId) {
+		this.proximoId = proximoId;
+	}*/
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getCpf() {
+		return cpf;
+	}
+	public void setCpf(int cpf) {
+		this.cpf = cpf;
+	}
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	public int getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(int telefone) {
+		this.telefone = telefone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public Date getDataAdmissao() {
+		return dataAdmissao;
+	}
+	public void setDataAdmissao(Date dataAdmissao) {
+		this.dataAdmissao = dataAdmissao;
+	}
+	public String getCargo() {
+		return cargo;
+	}
+	public void setCargo(String cargo) {
+		this.cargo = cargo;
+	}
+	public double getSalario() {
+		return salario;
+	}
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public boolean isAtivo() {
+		return ativo;
+	}
+	public void setAtivo(boolean ativo) {
+		this.ativo = ativo;
+	}
+	
+	
+	
 }
---
+++ src/main/java/model/Operador.java
@@ -0,0 +1,9 @@
+package model;
+
+public class Operador {
+
+	public Operador() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
